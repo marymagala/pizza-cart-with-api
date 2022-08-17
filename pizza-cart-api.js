7cfcab558476308d8308d0a775446eb84a832c63
unknown <marymagalam@gmail.com>
2022-08-17 15:42:10 +0200

    pizza-api

--- pizza-cart-api.js
+++ pizza-cart-api.js
@@ -26,7 +26,7 @@ document.addEventListener('alpine:init', () => {
       },
 
       showCart(){
-        const url = 'http://pizza-cart-api.herokuapp.com/api/pizza-cart/${this.cartId}/get';
+        const url = `http://pizza-cart-api.herokuapp.com/api/pizza-cart/${this.cartId}/get`;
 
         axios
         .get(url)
@@ -35,11 +35,19 @@ document.addEventListener('alpine:init', () => {
         });
       },
       
+      pizzaImage(pizza) {
+        return `./img/${pizza.size}.png`
+      },
+      
       message: 'Eating pizzas',
-      pizzas: [],
       username: 'mary',
+      pizzas: [],
       cartId: '',
       cart: {total:0},
+      payNow: false,
+      paymentAmount: 0,
+      paymentMessage: '',
+
 
       add(pizza) {
         const params = {
@@ -54,8 +62,55 @@ document.addEventListener('alpine:init', () => {
            this.showCart();
           })
           .catch(err => alert(err));
-        //alert(pizza.id)
-      }
+       // alert(pizza.id)
+      },
+
+      remove(pizza){
+        // /api/pizza-cart/remove
+        const params = {
+          cart_code : this.cartId,
+          pizza_id : pizza.id
+        }
+
+        axios
+          .post('https://pizza-cart-api.herokuapp.com/api/pizza-cart/remove', params)
+          .then(()=>{
+            this.message= "pizza removed from the cart"
+            this.showCart();
+          })
+          .catch(err=>alert(err));
+
+      },
+
+      pay(pizza){
+        const params = {
+          cart_code : this.cartId,
+        }
+        axios
+          .post('https://pizza-cart-api.herokuapp.com/api/pizza-cart/pay', params)
+          .then(()=>{
+              if(!this.paymentAmount){
+                  this.paymentMessage = 'No amount entered!'
+              }
+              else if(this.paymentAmount >= this.cart.total.toFixed(2)){
+                  this.paymentMessage = 'Payment Sucessful!'
+                  this.message= this.username  +" Paid!"
+                  setTimeout(() => {
+                      this.cart.total = ''
+                  }, 3000);
+              }else{
+                  this.paymentMessage = 'Sorry - that is not enough money!'
+                  setTimeout(() => {
+                      this.cart.total = ''
+                  }, 3000);
+              }
+          })
+          .catch(err=>alert(err));
+      },
+
+
+
+
     }
   })
 })
